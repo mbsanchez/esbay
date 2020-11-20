31363a028b81729417ae132d902eb0f9917318c3
Manuel Sanchez <mbsanchez@unet.edu.ve>
2020-11-20 15:53:47 +0100

    Added endpoints to register and list products

--- product-ms/app/app.py
+++ product-ms/app/app.py
@@ -16,5 +16,35 @@ models.create_table(app)
 def hello():
     return 'Hello, welcome to the ESBay Product API\n'
 
+@app.route('/api/product/create', methods=['POST'])
+def post_create():
+
+    name = request.form['name']
+    seller = request.form['seller']
+    price = request.form['price']
+
+    item = models.Product()
+    item.name = name
+    item.seller = seller
+    item.price = price
+
+    models.db.session.add(item)
+    models.db.session.commit()
+
+    response = jsonify({'message': 'Product added', 'product': item.to_json()})
+
+    return response
+
+@app.route('/api/products', methods=['GET'])
+def products():
+    data = []
+
+    for row in models.Product.query.all():
+        data.append(row.to_json())
+
+    response = jsonify({ 'results': data })
+
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
